e296154f77b99d91b40c4c25d5782a5d28a7e840
Yaseen Asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2023-12-19 10:07:30 +0200

    Add files via upload

---
+++ localizationConfig.ts
@@ -0,0 +1,9 @@
+import LocalizedStrings from 'react-localization';
+import * as arTranslations from './src/locales/ar.json';
+
+const localization = new LocalizedStrings({
+  ar: arTranslations,
+
+});
+
+export default localization;
